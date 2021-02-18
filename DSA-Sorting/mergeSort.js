ea377c4a9b348629301a56b5156e5df06321f189
Roxanne <roxanne-m@users.noreply.github.com>
2021-02-18 11:57:27 -0800

    Problem 1

--- DSA-Sorting/mergeSort.js
+++ DSA-Sorting/mergeSort.js
@@ -3,48 +3,46 @@
 // Divide & conquer. Breaks array down into continually smaller chunks, then merges them back together in correct order.
 // Best, Average, and Worst case: O(nlog(n)), and is better than bubble sort.
 
-function mergeSort(array){
-    // If array has 1 or 0 elements, then it is by definition sorted. Return array itself
-    if(array.length <= 1){
-        return array;
-    }
+function mergeSort(array) {
+  // If array has 1 or 0 elements, then it is by definition sorted. Return array itself
+  if (array.length <= 1) {
+    return array;
+  }
 
-    // Otherwise, slice array into 2 halves
-    const middle = Math.floor(array.length / 2);
-    let left = array.slice(0, middle);  // left half
-    let right = array.slice(middle, array.length);  // right half
+  // Otherwise, slice array into 2 halves
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle); // left half
+  let right = array.slice(middle, array.length); // right half
 
-    // recursively call mergeSort for each half
-    left = mergeSort(left);
-    right = mergeSort(right);
-    return merge(left, right, array);   //then merge together by calling merge function
+  // recursively call mergeSort for each half
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, array); //then merge together by calling merge function
 }
 
+function merge(left, right, array) {
+  // set indices to 0
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
 
-function merge(left, right, array){
-    // set indices to 0
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let outputIndex = 0;
-
-    while(leftIndex < left.length && rightIndex < right.length){
-        // if left value is less than right value add left to array, else add right to array
-        if(left[leftIndex] < right[rightIndex]){
-            array[outputIndex++] = left[leftIndex++];
-        }
-        else{
-            array[outputIndex++] = right[rightIndex++];
-        }
+  while (leftIndex < left.length && rightIndex < right.length) {
+    // if left value is less than right value add left to array, else add right to array
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    } else {
+      array[outputIndex++] = right[rightIndex++];
     }
+  }
 
-    // when 1 array is empty, you add all remaining values from the other array to it.
-    for(let i = leftIndex; i < left.length; i++){
-        array[outputIndex++] = left[i];
-    }
+  // when 1 array is empty, you add all remaining values from the other array to it.
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
 
-    for(let i = rightIndex; i < right.length; i++){
-        array[outputIndex++] = right[i];
-    }
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
 
-    return array;
-};
+  return array;
+}
--- DSA-Sorting/problem1.js
+++ DSA-Sorting/problem1.js
@@ -5,7 +5,27 @@ Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27,
 
 What is the resulting list that will be sorted after 3 recursive calls to mergesort?
 What is the resulting list that will be sorted after 16 recursive calls to mergesort?
+1: [21, 1, 26, 45, 29, 28, 2, 9] 
+2: [21, 1, 26, 45] 
+3: [21, 1] 
+4: [21]
+5: [1]
+6: [26, 45]
+7: [26]
+8: [45]
+9: [29, 28, 2, 9]
+10: [29, 28]
+11: [29]
+12: [28]
+13: [2, 9]
+14: [2]
+15: [9]
+16: [16, 49, 39, 27, 43, 34, 46, 40]
+
 What are the first 2 lists to be merged?
+[1, 21] + [26, 45]
+
 Which two lists would be merged on the 7th merge?
+The last two lists: [1, 2, 9, 21, 26, 28, 29, 45] & [16, 27, 34, 39, 40, 43, 46, 49]
 */
 
